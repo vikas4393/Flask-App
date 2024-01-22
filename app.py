a03221edf5463ff5244d7b552be43da2c6960532
durva2k22 <durvak2022@gmail.com>
2024-01-22 09:56:58 +0530

    Message Change

--- app.py
+++ app.py
@@ -16,14 +16,5 @@ def hello_there(name):
     now = datetime.now()
     formatted_now = now.strftime("%A, %d %B, %Y at %X")
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    content = "Hello there, " + name + "! It's " + formatted_now
     return content
